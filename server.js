8ecedc621d7df8eb909bf740d644e4057f7b7bec
Shimon242 <shimonbrathwaite8@gmail.com>
2026-06-09 00:17:35 -0500

    added cors

--- server.js
+++ server.js
@@ -108,6 +108,23 @@ app.post("/api/traces/reset", async (req, res) => {
   }
 });
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    }
+  })
+);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
